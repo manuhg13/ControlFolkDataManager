87f63bc28c41c428ddcfa5e956d45336fbdf40a8
manu <manuhg13@gmail.com>
2023-06-14 00:25:57 +0200

    add: first commit

---
+++ src/main/java/com/controlfolk/app/controlfolkdatamanager/ControlFolkDataManagerApplication.java
@@ -0,0 +1,13 @@
+package com.controlfolk.app.controlfolkdatamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlFolkDataManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlFolkDataManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/controlfolk/app/controlfolkdatamanager/ControlFolkDataManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlfolk.app.controlfolkdatamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlFolkDataManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
