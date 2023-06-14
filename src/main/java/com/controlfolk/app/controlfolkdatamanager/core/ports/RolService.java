5d88c0b69725bfd958f995ef462d4e398b41a05f
manu <manuhg13@gmail.com>
2023-06-14 22:49:35 +0200

    refactor: rol service

--- src/main/java/com/controlfolk/app/controlfolkdatamanager/core/ports/RolService.java
+++ src/main/java/com/controlfolk/app/controlfolkdatamanager/core/ports/RolService.java
@@ -4,11 +4,7 @@ import com.controlfolk.app.controlfolkdatamanager.core.models.Rol;
 import reactor.core.publisher.Mono;
 
 public interface RolService {
-
     Mono<Rol> create(Rol rol);
-
     Mono<Void> delete(Rol rol);
-
     Mono<Rol> findByRol(String rol);
-
 }
